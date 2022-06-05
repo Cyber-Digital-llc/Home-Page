5571b9022dabc85ca1138789c36c51b1931bcf90
Joe Hernandez <j.t.hernandez999@gmail.com>
2022-06-05 13:52:48 -0700

    Added mail to links to footer contact links

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,40 +1,52 @@
-import React from 'react';
-import cyberDigitalLogoWhite from '../../assets/cyberDigitalLogoWhite.png';
-import './footer.css';
+import React from 'react'
+import cyberDigitalLogoWhite from '../../assets/cyberDigitalLogoWhite.png'
+import './footer.css'
 
 const Footer = () => (
-  <div className="gpt3__footer section__padding">
+  <div className="gpt3__footer section__padding" id="contact">
     <div className="gpt3__footer-heading">
-      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+      <h1 className="gradient__text">
+        Do you want to step in to the future before others
+      </h1>
     </div>
 
     <div className="gpt3__footer-btn">
-      <p>Let us Chat</p>
+      <a href="mailto:cyberdigitalllc@gmail.com">
+        <p>Let us Chat</p>
+      </a>
     </div>
 
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
         <img src={cyberDigitalLogoWhite} alt="gpt3_logo" />
-        <p>Los Angeles, CA, <br /> All Rights Reserved</p>
+        <p>
+          Los Angeles, CA, <br /> All Rights Reserved
+        </p>
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Links</h4>
         <p>Overons</p>
         <p>Social Media</p>
         <p>Counters</p>
-        <p>Contact</p>
+        <a href="mailto:cyberdigitalllc@gmail.com">
+          <p>Contact</p>
+        </a>
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Company</h4>
         <p>Terms & Conditions </p>
         <p>Privacy Policy</p>
-        <p>Contact</p>
+        <a href="mailto:cyberdigitalllc@gmail.com">
+          <p>Contact</p>
+        </a>
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Get in touch</h4>
         <p>Los Angeles, CA</p>
         <p>626.786.7473</p>
-        <p>cyberdigitalllc@gmail.com</p>
+        <a href="mailto:cyberdigitalllc@gmail.com">
+          <p>cyberdigitalllc@gmail.com</p>
+        </a>
       </div>
     </div>
 
@@ -42,6 +54,6 @@ const Footer = () => (
       <p>@2022 Cyber Digital, llc. All rights reserved.</p>
     </div>
   </div>
-);
+)
 
-export default Footer;
+export default Footer
